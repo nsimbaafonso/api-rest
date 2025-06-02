78c9f9d427d60208e79b17d5d9ef40826847050f
Nsimba Afonso <nsimbaafonso0@gmail.com>
2025-06-02 11:36:09 +0100

    Arquivo atualizado

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 import app from "./src/app.js"
+import conexao from "./infra/conexao.js"
 const PORT = 3000 // definindo porta do projeto
 
-//escutando a porta 3000
-app.listen(PORT, () => {
-    console.log(`Servidor rodando no endereço http://localhost:${PORT}`)
+//fazer a conexao
+conexao.connect((error) => {
+    if (error) {
+        console.log("A conexão falhou:" + error)
+    } else {//só depois da conexão é que chamamos o servidor
+        console.log("Conexão feito com sucesso!")
+        //escutando a porta 3000
+        app.listen(PORT, () => {
+            console.log(`Servidor rodando no endereço http://localhost:${PORT}`)
+        })
+    }
 })
