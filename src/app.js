5f63224893cccdf8146160551ebede4ae03b314f
Nsimba Afonso <nsimbaafonso0@gmail.com>
2025-06-03 06:44:27 +0100

    Refatorando a lógica das rotas para trabalhar com dados do BD

--- src/app.js
+++ src/app.js
@@ -1,15 +1,9 @@
 //const express = require("express") // importando o express
 import express from "express" // importando o express
+import conexao from "../infra/conexao.js" // importando a conexão com o bd
+
 const app = express() // criando instância do express
 app.use(express.json()) // indicar para o express ler body da requisição com json
-
-// mock
-const selecoes = [
-    {id:1, selecao:'Brasil', grupo:'G'},
-    {id:2, selecao:'Suiça', grupo:'G'},
-    {id:3, selecao:'Camarões', grupo:'G'},
-    {id:4, selecao:'Sérvia', grupo:'G'},
-]
 
 // função que busca seleção por id
 function buscarSelecaoPorId(id) {
@@ -21,25 +15,48 @@ function buscarIndexSelecao(id) {
     return selecoes.findIndex(selecao => selecao.id == id)
 }
 
-//Rota padrão ou raiz| passamos dois paramêtros, req: de requisição e res: de resposta
-app.get("/", (req, res) => {
-    res.send("Curso de NodeJS feito em 2025")
-}) 
-
-//Busca seleção por id
-app.get("/selecoes/:id", (req, res) => {
-    res.json(buscarSelecaoPorId(req.params.id))
-})
+//Rotas 
 
 //Lista seleções
 app.get("/selecoes", (req, res) => {
-    res.status(200).send(selecoes)
+    const sql = "SELECT * FROM selecoes"
+    conexao.query(sql, (error, result) => {
+        if (error) {
+            console.log("Ocorreu um erro: "+error)
+            res.status(404).json({"erro": error})
+        } else {
+            res.status(200).json(result)
+        }
+    })
+})
+
+//Busca seleção por id
+app.get("/selecoes/:id", (req, res) => {
+    const id = req.params.id
+    const sql = "SELECT * FROM selecoes WHERE id = ?"
+    conexao.query(sql, id, (error, result) => {
+        const linha = result[0]
+        if (error) {
+            console.log("Ocorreu um erro: "+error)
+            res.status(404).json({"erro": error})
+        } else {
+            res.status(200).json(linha)
+        }
+    })
 })
 
 //Cadastra seleção
 app.post("/selecoes", (req, res) => {
-    selecoes.push(req.body)
-    res.status(201).send("Seleção cadastrada com sucesso")
+    const selecao = req.body
+    const sql = "INSERT INTO selecoes SET ?"
+    conexao.query(sql, selecao, (error, result) => {
+        if (error) {
+            console.log("Ocorreu um erro: "+error)
+            res.status(404).json({"erro": error})
+        } else {
+            res.status(201).json(result)
+        }
+    })
 })
 
 //Elimina seleção
