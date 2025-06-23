758e4dba6bc4f570f5434b320acbad9dc847f27d
Nsimba Afonso <nsimbaafonso0@gmail.com>
2025-06-23 22:44:42 +0100

    Códigos desnecessários retirados

--- src/app.js
+++ src/app.js
@@ -1,37 +1,21 @@
 import express from "express" 
-import conexao from "./app/database/conexao.js" 
 import SelecaoController from "./app/controllers/SelecaoController.js"
-
 
 const app = express()
 app.use(express.json())
-
-// função que busca seleção por id
-function buscarSelecaoPorId(id) {
-    return selecoes.filter(selecao => selecao.id == id)
-}
-
-// função que busca a posição da seleção por id
-function buscarIndexSelecao(id) {
-    return selecoes.findIndex(selecao => selecao.id == id)
-}
 
 //Rotas 
 
 //Lista seleções
 app.get("/selecoes", SelecaoController.index)
-
 //Busca seleção por id
 app.get("/selecoes/:id", SelecaoController.show)
-
 //Cadastra seleção
 app.post("/selecoes", SelecaoController.store)
-
+//Atualiza seleção
+app.put("/selecoes/:id", SelecaoController.update)
 //Elimina seleção
 app.delete("/selecoes/:id", SelecaoController.delete)
-
-//Atualiza seleção
-app.put("/selecoes/:id", )
 
 export default app
 //para rodar o projeto, vamos no terminal e digitamos: node src/app.js
