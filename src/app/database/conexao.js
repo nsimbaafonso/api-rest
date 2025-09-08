18d5c44868859d2a4fe56670812494c99fc1af31
Nsimba Afonso <nsimbaafonso0@gmail.com>
2025-09-08 08:24:35 +0100

    Função para consultas adicionada

--- src/app/database/conexao.js
+++ src/app/database/conexao.js
@@ -11,4 +11,21 @@ const conexao = mysql.createConnection({
 
 conexao.connect()
 
+/**
+ * Executa um código SQL com ou sem valores
+ * @param {string} sql instrução SQL a ser executada
+ * @param {string=id | [selecao, id]} valores valores a serem passados para o sql
+ * @param {string} mensagemReject mensagem a ser exibida
+ * @returns objeto da Promisse
+ */
+export const consulta = (sql, valores="", mensagemReject) => {
+    return new Promise((resolve, reject) => {
+        conexao.query(sql, valores, (error, result) => {
+            if (error) return reject(mensagemReject)
+            const row = JSON.parse(JSON.stringify(result))
+            return resolve(row)
+        })
+    })
+}
+
 export default conexao
