45484eacd32de93a11e21dd792475c2c1a727e39
Nsimba Afonso <nsimbaafonso0@gmail.com>
2025-09-08 08:30:14 +0100

    server movido

--- server.js
+++
@@ -1,7 +0,0 @@
-import app from "./src/app.js"
-const PORT = 3000 // definindo porta do projeto
-
-//escutando a porta 3000
-app.listen(PORT, () => {
-    console.log(`Servidor rodando no endereço http://localhost:${PORT}`)
-})
